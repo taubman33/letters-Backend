42593fbe255e80305b74749761875ed4c5b08450
nazeer171 <nazeer3886@gmail.com>
2021-03-15 14:43:28 -0500

    setting up urls

--- letter/views.py
+++ letter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Students
 # Create your views here.
+
+def student_list(request):
+    students = Students.objects.all()
+    return render(request, "letter/student_list.html", {students: students})
