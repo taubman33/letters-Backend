4fd27848317e9bc2eb3eb85aab24794cba2c1859
kel <kelpius21@gmail.com>
2021-03-15 17:12:55 -0400

    Backend is up and functional. Deployment is next.

---
+++ letter/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Students
+
+class StudentsSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Students
+        fields = ('id', 'initials', 'location', 'letter',)
